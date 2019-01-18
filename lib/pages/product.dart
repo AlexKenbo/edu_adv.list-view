c3cebaf179fdb771aef4769dd2f0baf5b08cf6f1
Alex Vasiliev <alex@mdi-toys.ru>
2019-01-18 09:30:09 +0300

    Улучшение Detail Page

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -2,20 +2,30 @@ import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Product Detail'),
         ),
-        body: Column(
+        body: Center(
+            child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center, // выравнивание по вертикали. не используем тк с картинкой по центру не красиво
+          crossAxisAlignment: CrossAxisAlignment
+              .center, // выравнивание по горизонтали не сработало, так как ширина столбца = ширине элемента
           children: <Widget>[
-            Text('On the Product Page'),
-            RaisedButton(
-              child: Text('BACK'),
-              onPressed: () => Navigator.pop(context),
-            )
+            Image.asset(
+                '/Users/santes/Documents/flutter_projects/list-view/assets/food.jpg'),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text('On the Product Page')),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('BACK'),
+                  onPressed: () => Navigator.pop(context),
+                ))
           ],
-        ) 
-      );
+        )));
   }
 }
