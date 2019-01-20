7b247245539ed9b8195549324abd6882ee915c02
Alex Vasiliev <alex@mdi-toys.ru>
2019-01-20 19:57:26 +0300

    Добавление модального окна

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -4,7 +4,19 @@ class ProductCreatePage extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Container(child: Center(
-      child: Text('Create Product'),
+      child: RaisedButton(
+        child: Text('Save'),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context){
+              return Center(
+                child: Text('This is a Modal!'),
+              );
+            }
+          );
+        },
+      ),
     ),);
   }
 }
