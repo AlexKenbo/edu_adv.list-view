1c8c43b9221360664bc7dae5dc0eafa010285733
Alex Vasiliev <alex@mdi-toys.ru>
2019-01-24 09:32:34 +0300

    Добавление иконок в сайдбар

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -23,6 +23,7 @@ class ProductsAdminPage extends StatelessWidget {
                   title: Text('Choose'),
                 ),
                 ListTile(
+                  leading: Icon(Icons.shop),
                   title: Text('All products'),
                   onTap: () {
                     Navigator.pushReplacementNamed(context, '/products');
