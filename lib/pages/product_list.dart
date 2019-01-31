8db01d95c1a4b1a9a3b8edfc65fb35a7639a4232
Васильев Александр <kenbo@MacBook-Pro-Vasilev.local>
2019-01-31 09:17:52 +0300

    Улучшение ListTile

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -11,26 +11,28 @@ class ProductListPage extends StatelessWidget {
   Widget build(BuildContext context){
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        return ListTile( 
-          leading: Image(
-            image: AssetImage(products[index]['image']), 
-            height: 96.0, 
-            width: 96.0, 
-            fit: BoxFit.fitWidth,
-            ),
-          title: Text(products[index]['title']),
-          trailing: IconButton(
-            icon: Icon(Icons.edit),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return ProductEditPage(product: products[index], updateProduct: updateProduct, productIndex: index,);
-                  }
-                )
-              );
-            },
-            ),
+        return Column(children: <Widget>[
+            ListTile( 
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(products[index]['image']), 
+              ),
+            title: Text(products[index]['title']),
+            subtitle: Text('\$${products[index]['price']}'),
+            trailing: IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return ProductEditPage(product: products[index], updateProduct: updateProduct, productIndex: index,);
+                    }
+                  )
+                );
+              },
+              ),
+          ),
+          Divider()
+        ],
         );
       },
       itemCount: products.length,
