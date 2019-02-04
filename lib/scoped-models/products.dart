f7eeab7f3e585fd6d6a950f263b602b2c71c0c80
Alex Vasiliev <alex@mdi-toys.ru>
2019-02-04 08:54:45 +0300

    Создание Favorite - доб-е свойства и создание метода переключения

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -1,7 +1,7 @@
 import 'package:scoped_model/scoped_model.dart';
 import '../models/product.dart';
 
-class ProductsModel extends Model{
+class ProductsModel extends Model {
   List<Product> _products = [];
   int _selectedProductIndex;
 
@@ -14,15 +14,15 @@ class ProductsModel extends Model{
   }
 
   Product get selectedProduct {
-    if (_selectedProductIndex == null){
+    if (_selectedProductIndex == null) {
       return null;
     }
     return _products[_selectedProductIndex];
   }
 
   void addProduct(Product product) {
-      _products.add(product);
-      _selectedProductIndex = null;
+    _products.add(product);
+    _selectedProductIndex = null;
   }
 
   void updateProduct(Product product) {
@@ -32,6 +32,19 @@ class ProductsModel extends Model{
 
   void deleteProduct() {
     _products.removeAt(_selectedProductIndex);
+    _selectedProductIndex = null;
+  }
+
+  void toggleProductFavoriteStatus() {
+    final bool isCurrentlyFavorite = selectedProduct.isFavorite;
+    final bool newFavoriteStatus = !isCurrentlyFavorite;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        image: selectedProduct.image,
+        price: selectedProduct.price,
+        isFavorite: newFavoriteStatus);
+    _products[_selectedProductIndex] = updatedProduct;
     _selectedProductIndex = null;
   }
 
