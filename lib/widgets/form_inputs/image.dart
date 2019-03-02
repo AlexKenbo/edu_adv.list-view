6e6f34fd2de55f54af05518cb1adb319e7e71022
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-02 12:56:44 +0300

    Добавление кнопок Use Camera и Use Gallery

--- lib/widgets/form_inputs/image.dart
+++ lib/widgets/form_inputs/image.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/material.dart';
 
 class ImageInput extends StatefulWidget {
-  final Widget child;
-
-  ImageInput({Key key, this.child}) : super(key: key);
-
+  @override
   _ImageInputState createState() => _ImageInputState();
 }
 
 class _ImageInputState extends State<ImageInput> {
+  void _openImagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 250.0,
+            padding: EdgeInsets.all(10.0),
+            child: Column(
+              children: <Widget>[
+                Text('Pick an Image', style: TextStyle(fontWeight: FontWeight.bold),),
+                SizedBox(
+                  height: 15.0,
+                ),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text('Use Camera'),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  textColor: Theme.of(context).primaryColor,
+                  child: Text('Use Gallery'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     final buttonColor = Theme.of(context).accentColor;
     return Column(
       children: <Widget>[
         OutlineButton(
-          borderSide: BorderSide(color: Theme.of(context).accentColor, width: 2.0),
-          onPressed: (){
-            
+          borderSide:
+              BorderSide(color: Theme.of(context).accentColor, width: 2.0),
+          onPressed: () {
+            _openImagePicker(context);
           },
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -25,8 +52,11 @@ class _ImageInputState extends State<ImageInput> {
               Icon(Icons.camera_alt, color: buttonColor),
               SizedBox(
                 width: 5.0,
-              ), 
-              Text('Add Image', style: TextStyle(color: buttonColor),)
+              ),
+              Text(
+                'Add Image',
+                style: TextStyle(color: buttonColor),
+              )
             ],
           ),
         )
