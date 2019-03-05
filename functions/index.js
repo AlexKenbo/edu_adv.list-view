8c00a87462e17e6b39fe1844aeac49ec22f817c1
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-05 08:14:50 +0300

    Добавление endpoint для Firebase Function API

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +7,18 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.storeImage = functions.https.onRequest((req, res) => {
+    return cors(req, res, () => {
+        if (req.method !== 'POST') {
+            return res.status(500).json({ message: 'Not allowed.' });
+        }
+
+        if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+            return res.status(401).json({ error: 'Unauthorised.' })
+        }
+
+        let idToken;
+        idToken = req.headers.authorization.split('Bearer ')[1];
+    });
+});
