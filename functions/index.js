2b2a4f07c13e0aa65e5bafa60f02d2996f6ab18a
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-05 08:35:14 +0300

    Endpoint API - добавление чтения файлов и полей.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const os = require('os');
+const path = require('path');
+const fs = require('fs'); 
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -20,5 +24,23 @@ exports.storeImage = functions.https.onRequest((req, res) => {
 
         let idToken;
         idToken = req.headers.authorization.split('Bearer ')[1];
+
+        const busboy = new Busboy({headers: req.headers});
+        let uploadData;
+        let oldImagePath;
+
+        busboy.on('file', (fieldname, file, encoding, mimetype) => {
+            const filePath = path.join(os.tmpdir(), filename);
+            uploadData = {filePath: filePath, type: mimetype, name: filename};
+            file.pipe(fs.createWriteStream(filePath));
+        });
+
+        busboy.on('field', (fieldname, value) => {
+            oldImagePath = decodeURIComponent(value);
+        });
+
+        busboy.on('finish', () => {
+            
+        });
     });
 });
