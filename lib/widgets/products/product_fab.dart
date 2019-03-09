33f5a2c2b1f3d3831280e0822c782f1600189750
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-09 10:05:39 +0300

    Анимация кнопки настроек

--- lib/widgets/products/product_fab.dart
+++ lib/widgets/products/product_fab.dart
@@ -1,3 +1,4 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 
 import 'package:scoped_model/scoped_model.dart';
@@ -20,10 +21,7 @@ class _ProductFABState extends State<ProductFAB> with TickerProviderStateMixin {
   @override
   void initState() {
     _controller =
-        AnimationController(
-          vsync: this, 
-          duration: Duration(milliseconds: 200)
-        );
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
     super.initState();
   }
 
@@ -37,50 +35,50 @@ class _ProductFABState extends State<ProductFAB> with TickerProviderStateMixin {
             width: 56.0,
             alignment: FractionalOffset.topCenter,
             child: ScaleTransition(
-              scale: CurvedAnimation(
-                parent: _controller,
-                curve: Interval(0.0, 1.0, curve: Curves.easeOut),
-              ),
-              child: FloatingActionButton(
-                backgroundColor: Theme.of(context).cardColor,
-                heroTag: 'contact',
-                mini: true,
-                onPressed: () async {
-                  final url = 'mailto:${widget.product.userEmail}';
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  } else {
-                    throw 'Could not launch!';
-                  }
-                },
-                child: Icon(
-                  Icons.mail,
-                  color: Theme.of(context).primaryColor,
+                scale: CurvedAnimation(
+                  parent: _controller,
+                  curve: Interval(0.0, 1.0, curve: Curves.easeOut),
                 ),
-            ))),
+                child: FloatingActionButton(
+                  backgroundColor: Theme.of(context).cardColor,
+                  heroTag: 'contact',
+                  mini: true,
+                  onPressed: () async {
+                    final url = 'mailto:${widget.product.userEmail}';
+                    if (await canLaunch(url)) {
+                      await launch(url);
+                    } else {
+                      throw 'Could not launch!';
+                    }
+                  },
+                  child: Icon(
+                    Icons.mail,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ))),
         Container(
             height: 70.0,
             width: 56.0,
             alignment: FractionalOffset.topCenter,
             child: ScaleTransition(
-              scale: CurvedAnimation(
-                parent: _controller,
-                curve: Interval(0.0, 0.5, curve: Curves.easeOut),
-              ),
-              child: FloatingActionButton(
-                backgroundColor: Theme.of(context).cardColor,
-                heroTag: 'favorite',
-                mini: true,
-                onPressed: () {
-                  model.toggleProductFavoriteStatus();
-                },
-                child: Icon(
-                  model.selectedProduct.isFavorite
-                      ? Icons.favorite
-                      : Icons.favorite_border,
-                  color: Colors.red[300],
+                scale: CurvedAnimation(
+                  parent: _controller,
+                  curve: Interval(0.0, 0.5, curve: Curves.easeOut),
                 ),
-            ))),
+                child: FloatingActionButton(
+                  backgroundColor: Theme.of(context).cardColor,
+                  heroTag: 'favorite',
+                  mini: true,
+                  onPressed: () {
+                    model.toggleProductFavoriteStatus();
+                  },
+                  child: Icon(
+                    model.selectedProduct.isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border,
+                    color: Colors.red[300],
+                  ),
+                ))),
         Container(
             height: 70.0,
             width: 56.0,
@@ -94,7 +92,14 @@ class _ProductFABState extends State<ProductFAB> with TickerProviderStateMixin {
                   _controller.reverse();
                 }
               },
-              child: Icon(Icons.more_vert),
+              child: AnimatedBuilder(
+                animation: _controller,
+                builder: (BuildContext context, Widget child) {
+                  return Transform(
+                    alignment: FractionalOffset.center,
+                    transform: Matrix4.rotationZ(_controller.value * 0.5 * math.pi),
+                    child: Icon(_controller.isDismissed ? Icons.more_vert : Icons.close));
+              }),
             )),
       ]);
     });
