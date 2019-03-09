45645168f84532a8db28fa25380ae094986d1587
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-09 09:48:35 +0300

    Анимации для кнопок contact и favorite

--- lib/widgets/products/product_fab.dart
+++ lib/widgets/products/product_fab.dart
@@ -14,7 +14,19 @@ class ProductFAB extends StatefulWidget {
   _ProductFABState createState() => _ProductFABState();
 }
 
-class _ProductFABState extends State<ProductFAB> {
+class _ProductFABState extends State<ProductFAB> with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(
+          vsync: this, 
+          duration: Duration(milliseconds: 200)
+        );
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant(
@@ -24,48 +36,64 @@ class _ProductFABState extends State<ProductFAB> {
             height: 70.0,
             width: 56.0,
             alignment: FractionalOffset.topCenter,
-            child: FloatingActionButton(
-              backgroundColor: Theme.of(context).cardColor,
-              heroTag: 'contact',
-              mini: true,
-              onPressed: () async {
-                final url = 'mailto:${widget.product.userEmail}';
-                if (await canLaunch(url)) {
-                  await launch(url);
-                } else {
-                  throw 'Could not launch!';
-                }
-              },
-              child: Icon(
-                Icons.mail,
-                color: Theme.of(context).primaryColor,
+            child: ScaleTransition(
+              scale: CurvedAnimation(
+                parent: _controller,
+                curve: Interval(0.0, 1.0, curve: Curves.easeOut),
               ),
-            )),
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).cardColor,
+                heroTag: 'contact',
+                mini: true,
+                onPressed: () async {
+                  final url = 'mailto:${widget.product.userEmail}';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  } else {
+                    throw 'Could not launch!';
+                  }
+                },
+                child: Icon(
+                  Icons.mail,
+                  color: Theme.of(context).primaryColor,
+                ),
+            ))),
         Container(
             height: 70.0,
             width: 56.0,
             alignment: FractionalOffset.topCenter,
-            child: FloatingActionButton(
-              backgroundColor: Theme.of(context).cardColor,
-              heroTag: 'favorite',
-              mini: true,
-              onPressed: () {
-                model.toggleProductFavoriteStatus();
-              },
-              child: Icon(
-                model.selectedProduct.isFavorite
-                    ? Icons.favorite
-                    : Icons.favorite_border,
-                color: Colors.red[300],
+            child: ScaleTransition(
+              scale: CurvedAnimation(
+                parent: _controller,
+                curve: Interval(0.0, 1.0, curve: Curves.easeOut),
               ),
-            )),
+              child: FloatingActionButton(
+                backgroundColor: Theme.of(context).cardColor,
+                heroTag: 'favorite',
+                mini: true,
+                onPressed: () {
+                  model.toggleProductFavoriteStatus();
+                },
+                child: Icon(
+                  model.selectedProduct.isFavorite
+                      ? Icons.favorite
+                      : Icons.favorite_border,
+                  color: Colors.red[300],
+                ),
+            ))),
         Container(
             height: 70.0,
             width: 56.0,
             child: FloatingActionButton(
               heroTag: 'options',
               mini: true,
-              onPressed: () {},
+              onPressed: () {
+                if (_controller.isDismissed) {
+                  _controller.forward();
+                } else {
+                  _controller.reverse();
+                }
+              },
               child: Icon(Icons.more_vert),
             )),
       ]);
