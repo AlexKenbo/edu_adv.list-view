fa3a2bc75084a774d7a1b7951191e5757fd4148c
Alex Vasiliev <alex@mdi-toys.ru>
2019-03-12 16:35:22 +0300

    Добавление иконок, добавление заставок загрузки

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,7 @@ class _MyAppState extends State<MyApp> {
     return ScopedModel<MainModel>(
         model: _model,
         child: MaterialApp(
+          title: 'EasyList',
           routes: {
             '/': (BuildContext context) => !_isAuthenticated
                 ? AuthPage()
